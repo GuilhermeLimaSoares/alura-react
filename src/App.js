d8b3faffeee16b756714a297e441d115a957f986
GuilhermeLimaSoares <guilhermelimasoares9@gmail.com>
2020-02-23 22:32:11 -0300

    Refatoracao

--- src/App.js
+++ src/App.js
@@ -2,19 +2,23 @@ import React, { Component} from 'react';
 import './css/pure-min.css';
 import './css/side-menu.css';
 import $ from 'jquery';
-import InputCustomizado from './components/InputCustomizado';
+import {FormularioAutor, TabelaAutores} from './Autor'
 // function App() {
 
 class App extends Component{ 
 
   constructor() {
     super();
+    this.state = {lista : []};
+
+    ///antes da refatoração
+
     // this.state = {lista: [{nome:'Waldemar', email:'alberto.souza@caelum.com.br', senha:'123456'}]};
-    this.state = {lista: [], nome:'', email:'', senha:''};
-    this.enviaForm = this.enviaForm.bind(this);
-    this.setNome = this.setNome.bind(this);
-    this.setEmail = this.setEmail.bind(this);
-    this.setSenha = this.setSenha.bind(this);
+    // this.state = {lista: [], nome:'', email:'', senha:''};
+    // this.enviaForm = this.enviaForm.bind(this);
+    // this.setNome = this.setNome.bind(this);
+    // this.setEmail = this.setEmail.bind(this);
+    // this.setSenha = this.setSenha.bind(this);
     // $.ajax({
     //   url:"http://cdc-react.herokuapp.com/api/autores",
     //   dataType: 'json',
@@ -38,53 +42,53 @@ class App extends Component{
   //   });
   // }
 
-  componentDidMount(){ // chamado depois da primeira reenderização
-    console.log('DidMount');
-    $.ajax({
-      url:"http://cdc-react.herokuapp.com/api/autores",
-      dataType: 'json',
-      success: function(resposta){
-        // console.log(resposta);
-        console.log('chamou a resposta');
-        console.log(resposta);
-        // this.state = {lista:resposta};
-        this.setState({lista:resposta});
-      }.bind(this)
-    });
-  }
+  // componentDidMount(){ // chamado depois da primeira reenderização
+  //   console.log('DidMount');
+  //   $.ajax({
+  //     url:"http://cdc-react.herokuapp.com/api/autores",
+  //     dataType: 'json',
+  //     success: function(resposta){
+  //       // console.log(resposta);
+  //       console.log('chamou a resposta');
+  //       console.log(resposta);
+  //       // this.state = {lista:resposta};
+  //       this.setState({lista:resposta});
+  //     }.bind(this)
+  //   });
+  // }
 
-  enviaForm(event){
-    console.log('dados enviados');
-    console.log(event);
-    event.preventDefault();
-    $.ajax({
-      url:"http://cdc-react.herokuapp.com/api/autores",
-      contentType:'application/json',
-      dataType: 'json',
-      type:'post',
-      data: JSON.stringify({nome:this.state.nome, email:this.state.email, senha:this.state.senha}),
-      success: function(resposta){
-        // console.log(resposta);
-        console.log('dados enviados com sucesso');
-        this.setState({lista:resposta});
-      }.bind(this),
-      error: function(resposta){
-      console.log('error');
-      }
-    })
-  }
+  // enviaForm(event){
+  //   console.log('dados enviados');
+  //   console.log(event);
+  //   event.preventDefault();
+  //   $.ajax({
+  //     url:"http://cdc-react.herokuapp.com/api/autores",
+  //     contentType:'application/json',
+  //     dataType: 'json',
+  //     type:'post',
+  //     data: JSON.stringify({nome:this.state.nome, email:this.state.email, senha:this.state.senha}),
+  //     success: function(resposta){
+  //       // console.log(resposta);
+  //       console.log('dados enviados com sucesso');
+  //       this.setState({lista:resposta});
+  //     }.bind(this),
+  //     error: function(resposta){
+  //     console.log('error');
+  //     }
+  //   })
+  // }
 
-  setNome(evento){
-    this.setState({nome:evento.target.value});
-  }
+  // setNome(evento){
+  //   this.setState({nome:evento.target.value});
+  // }
 
-  setEmail(evento){
-    this.setState({email:evento.target.value});
-  }
+  // setEmail(evento){
+  //   this.setState({email:evento.target.value});
+  // }
 
-  setSenha(evento){
-    this.setState({senha:evento.target.value});
-  }
+  // setSenha(evento){
+  //   this.setState({senha:evento.target.value});
+  // }
 
 render(){
   console.log('render');
@@ -106,84 +110,15 @@ render(){
         </div>
     </div>
 
-    <div id="menu">
-        <div className="pure-menu">
-            <a className="pure-menu-heading" href="#">Company</a>
-
-            <ul className="pure-menu-list">
-                <li className="pure-menu-item"><a href="#" className="pure-menu-link">Home</a></li>
-                <li className="pure-menu-item"><a href="#" className="pure-menu-link">Autor</a></li>
-                <li className="pure-menu-item"><a href="#" className="pure-menu-link">Livro</a></li>
-
-                
-            </ul>
+    <div id="main">
+        <div className="header">
+          <h1>Cadastro de Autores</h1>
         </div>
-    </div>
-
-        <div id="main">
-            <div className="header">
-              <h1>Cadastro de Autores</h1>
-            </div>
-            <div className="content" id="content">
-              <div className="pure-form pure-form-aligned">
-                {/* <form className="pure-form pure-form-aligned" onSubmit={this.enviaForm.bind(this)} method="post">
-                  <div className="pure-control-group">
-                    <label htmlFor="nome">Nome</label> 
-                    <input id="nome" type="text" name="nome" value={this.state.nome}  onChange={this.setNome}/>                  
-                  </div>
-                  <div className="pure-control-group">
-                    <label htmlFor="email">Email</label> 
-                    <input id="email" type="email" name="email" value={this.state.email}  onChange={this.setEmail}/>                  
-                  </div>
-                  <div className="pure-control-group">
-                    <label htmlFor="senha">Senha</label> 
-                    <input id="senha" type="password" name="senha" value={this.state.senha} onChange={this.setSenha}/>                                      
-                  </div>
-                  <div className="pure-control-group">                                  
-                    <label></label> 
-                    <button type="submit" className="pure-button pure-button-primary">Gravar</button>                                    
-                  </div>
-          
-                </form>              */}
-
-                <form className="pure-form pure-form-aligned" onSubmit={this.enviaForm} method="post">
-                  <InputCustomizado id="nome" type="text" name="nome" value={this.state.nome} onChange={this.setNome} label="Nome"/>                                              
-                  <InputCustomizado id="email" type="email" name="email" value={this.state.email} onChange={this.setEmail} label="Email"/>                                              
-                  <InputCustomizado id="senha" type="password" name="senha" value={this.state.senha} onChange={this.setSenha} label="Senha"/>                                          
-                  <div className="pure-control-group">   
-                    <label></label>
-                    <button type="submit" className="pure-button pure-button-primary">Gravar</button>                                    
-                  </div>
-                </form>
-
-              </div>  
-              <div>            
-                <table className="pure-table">
-                  <thead>
-                    <tr>
-                      <th>Nome</th>
-                      <th>email</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {/* <tr>
-                      <td>Alberto</td>                
-                      <td>alberto.souza@caelum.com.br</td>                
-                    </tr> */
-                    this.state.lista.map(function(autor){
-                      return (
-                          <tr key={autor.id}>
-                            <td>{autor.nome}</td>
-                            <td>{autor.email}</td>
-                          </tr>
-                        );
-                    })
-                    }
-                  </tbody>
-                </table> 
-              </div>             
-            </div>
-          </div>            
+        <div className="content" id="content">
+          <FormularioAutor/>
+          <TabelaAutores/>           
+        </div>
+      </div>            
 </div>
   );
 }
---
+++ src/Autor.js
@@ -0,0 +1,115 @@
+import React, { Component } from 'react';
+import $ from 'jquery';
+import InputCustomizado from './components/InputCustomizado';
+
+export class FormularioAutor extends Component{
+    constructor() {
+        super();
+        this.state = {lista : [],nome:'',email:'', senha:''};
+        this.enviaForm = this.enviaForm.bind(this);
+        this.setNome = this.setNome.bind(this);
+        this.setEmail = this.setEmail.bind(this);
+        this.setSenha = this.setSenha.bind(this);
+      }
+
+      enviaForm(event){
+        console.log('dados enviados');
+        console.log(event);
+        event.preventDefault();
+        $.ajax({
+          url:"http://cdc-react.herokuapp.com/api/autores",
+          contentType:'application/json',
+          dataType: 'json',
+          type:'post',
+          data: JSON.stringify({nome:this.state.nome, email:this.state.email, senha:this.state.senha}),
+          success: function(resposta){
+            // console.log(resposta);
+            console.log('dados enviados com sucesso');
+            this.setState({lista:resposta});
+          }.bind(this),
+          error: function(resposta){
+          console.log('error');
+          }
+        })
+      }
+    
+      setNome(evento){
+        this.setState({nome:evento.target.value});
+      }
+    
+      setEmail(evento){
+        this.setState({email:evento.target.value});
+      }
+    
+      setSenha(evento){
+        this.setState({senha:evento.target.value});
+      }
+
+      render() {
+        return (
+            <div className="pure-form pure-form-aligned">
+                <form className="pure-form pure-form-aligned" onSubmit={this.enviaForm} method="post">
+                    <InputCustomizado id="nome" type="text" name="nome" value={this.state.nome} onChange={this.setNome} label="Nome"/>                                              
+                    <InputCustomizado id="email" type="email" name="email" value={this.state.email} onChange={this.setEmail} label="Email"/>                                              
+                    <InputCustomizado id="senha" type="password" name="senha" value={this.state.senha} onChange={this.setSenha} label="Senha"/>                                          
+                    <div className="pure-control-group">   
+                        <label></label>
+                        <button type="submit" className="pure-button pure-button-primary">Gravar</button>                                    
+                    </div>
+                </form>
+          </div>  
+        )
+    }
+}
+
+export class TabelaAutores extends Component{
+
+    constructor() {
+        super();
+        this.state = {lista : []};
+      }
+    
+  componentDidMount(){ // chamado depois da primeira reenderização
+    console.log('DidMount');
+    $.ajax({
+      url:"http://cdc-react.herokuapp.com/api/autores",
+      dataType: 'json',
+      success: function(resposta){
+        // console.log(resposta);
+        console.log('chamou a resposta');
+        console.log(resposta);
+        // this.state = {lista:resposta};
+        // this.setState({lista:resposta});
+        this.setState({lista:resposta.filter((row) => {
+            return row.nome.search(/[a-z]/g) === 0;
+        })});
+      }.bind(this)
+    });
+  }
+        render() {
+            return(
+                    <div>            
+                          <table className="pure-table">
+                            <thead>
+                              <tr>
+                                <th>Nome</th>
+                                <th>email</th>
+                              </tr>
+                            </thead>
+                            <tbody>
+                              {
+                                this.state.lista.map(function(autor){
+                                  return (
+                                    <tr key={autor.id}>
+                                      <td>{autor.nome}</td>
+                                      <td>{autor.email}</td>
+                                    </tr>
+                                  );
+                                })
+                              }
+                            </tbody>
+                          </table>
+                        </div>
+          );
+        }
+}
